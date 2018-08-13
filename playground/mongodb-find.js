4433180d0724c3f64ab4fd6fe913007fe1c578ac
Gowtham Karnan <gowthamkarnan.prg@gmail.com>
2018-08-13 15:06:18 +0530

    Add delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -15,11 +15,22 @@ MongoClient.connect('mongodb://localhost:27017/', (err, db) => {
   // },(err)=>{
   //     console.log('Unable to fetch data',err);
   // });
-  dbo.collection('Todos').find().count().then((count)=>{
-    console.log(`TOdos count ${count}`);     
-},(err)=>{
-    console.log('Unable to fetch data',err);
-});
+//   dbo.collection('Todos').find().count().then((count)=>{
+//     console.log(`Todos count ${count}`);     
+// },(err)=>{
+//     console.log('Unable to fetch data',err);
+// });
+//   dbo.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+//     console.log(JSON.stringify(result,undefined,2));
+    
+//   },(err)=>{
+//     console.log(err);
+    
+//   });
+  dbo.collection('Users').deleteMany({name:'Gowtham K'}).then((result)=>{
+    console.log(JSON.stringify(result,undefined,2));
+    
+  });
 
 
   db.close();
