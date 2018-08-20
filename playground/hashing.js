477f0127b325010768e373e2137a0e8a5bbebb7a
Gowtham Karnan <gowthamkarnan.prg@gmail.com>
2018-08-20 16:20:49 +0530

    Hash user apssword before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,10 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
-var token = jwt.sign(data,'secret');
-console.log(token);
-var decoded = jwt.verify(token,'secret');
-console.log('DECODED:',decoded);
+var password = '123abc!';
+
+bcrypt.genSalt(15,(err,salt)=>{
+    console.log('salt',salt);
+    
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+        
+    });
+});
+
+var hashedPassword = '$2a$15$KvZW3kbqTNSr36Qc01.7gOva.AFkteOwVPKTAZwHzfBLixTT7QMj.';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);    
+});
+
+// var data = {
+//     id:10
+// };
+// var token = jwt.sign(data,'secret');
+// console.log(token);
+// var decoded = jwt.verify(token,'secret');
+// console.log('DECODED:',decoded);
